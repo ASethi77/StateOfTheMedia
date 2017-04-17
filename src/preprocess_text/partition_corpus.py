d5a9238fba11da252a1c5be4867af5dc2e3c1896
dowling3 <drewdowling37@gmail.com>
2017-04-16 20:25:59 -0700

    we have bags of words

--- src/preprocess_text/partition_corpus.py
+++ src/preprocess_text/partition_corpus.py
@@ -1,3 +1,24 @@
 from preprocess_text.setup_corpus import setup_dev_corpus
+from dateutil.parser import parse
+from collections import defaultdict
+from random import shuffle
+import textacy
 
-corpra = setup_dev_corpus()
+corpus = textacy.Corpus.load(path="../../data/TextacyCorpra", name="WebHoseDevCorpus")
+
+date_to_docs = defaultdict(list)
+
+for doc in corpus.docs:
+    datestr = doc.metadata['published']
+    datetime_object = parse(datestr)
+
+    date_to_docs[datetime_object.date()].append(doc)
+
+# print(date_to_docs)
+for date, docs in date_to_docs.items():
+    # print(docs)
+    corpus_for_today = textacy.Corpus('en', docs=docs)
+    bag_o_words = corpus_for_today.word_freqs(as_strings=True)
+
+    print(bag_o_words)
+
