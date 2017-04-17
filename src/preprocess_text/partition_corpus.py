1ac7afe9e56604770926ed6bf0f55a2aa95feda5
dowling3 <drewdowling37@gmail.com>
2017-04-16 20:56:47 -0700

    we have bags of words for today

--- src/preprocess_text/partition_corpus.py
+++ src/preprocess_text/partition_corpus.py
@@ -17,8 +17,24 @@ for doc in corpus.docs:
 # print(date_to_docs)
 for date, docs in date_to_docs.items():
     # print(docs)
-    corpus_for_today = textacy.Corpus('en', docs=docs)
-    bag_o_words = corpus_for_today.word_freqs(as_strings=True)
+    # corpus_for_today = textacy.Corpus('en', docs=docs)
+    # for doc in corpus_for_today:
+    #     print(doc.text)
+    # bag_o_words = corpus_for_today.word_freqs(as_strings=True)
+    # print(bag_o_words)
+    # print()
+    bag_o_terms_today = dict()
+    bot_list = []
+    for doc in docs:
+        bag_o_terms = doc.to_bag_of_terms(named_entities=False, as_strings=True)
+        bot_list.append(bag_o_terms)
 
-    print(bag_o_words)
+    for bag_o_terms in bot_list:
+        for term, count in bag_o_terms.items():
+            if term not in bag_o_terms_today:
+                bag_o_terms_today[term] = count
+            else:
+                bag_o_terms_today[term] += count
+
+    print(bag_o_terms_today)
 
