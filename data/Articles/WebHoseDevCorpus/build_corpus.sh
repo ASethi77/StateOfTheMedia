c1cd7d6133f33d8df70fdfae30495ccc94980246
Aakash Sethi <aakash.k.sethi@gmail.com>
2017-04-18 14:35:36 -0700

    Formatted build_corpus.sh

--- data/Articles/WebHoseDevCorpus/build_corpus.sh
+++ data/Articles/WebHoseDevCorpus/build_corpus.sh
@@ -1,1 +1,9 @@
-for article in `ls -1 /opt/nlp_shared/data/news_articles/webhose_english_dataset/ | shuf`; do cp "/opt/nlp_shared/data/news_articles/webhose_english_dataset/$article" .; ((i++)); if [[ "$i" -gt "100" ]]; then break; fi; done
+NUM_ARTICLES=100
+
+for article in `ls -1 /opt/nlp_shared/data/news_articles/webhose_english_dataset/ | shuf`; do
+	cp "/opt/nlp_shared/data/news_articles/webhose_english_dataset/$article" .;
+	((i++));
+	if [[ "$i" -gt "${NUM_ARTICLES}" ]]; then
+		break;
+	fi
+done
