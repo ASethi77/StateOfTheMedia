293fa948a2dc070c4ab7faff6cb19818a306b67e
Aakash Sethi <aakash.k.sethi@gmail.com>
2017-04-18 15:59:20 -0700

    Fixed bugs in corpus builder script

--- data/Articles/WebHoseDevCorpus/build_corpus.sh
+++ data/Articles/WebHoseDevCorpus/build_corpus.sh
@@ -1,7 +1,10 @@
-NUM_ARTICLES=100
-
+NUM_ARTICLES=16275
+dataset_dir_name="WebHoseDataset-${NUM_ARTICLES}-Articles"
+rm -rf "$dataset_dir_name" >> /dev/null 2>&1
+mkdir $dataset_dir_name
+i=1
 for article in `ls -1 /opt/nlp_shared/data/news_articles/webhose_english_dataset/ | shuf`; do
-	cp "/opt/nlp_shared/data/news_articles/webhose_english_dataset/$article" .;
+	cp "/opt/nlp_shared/data/news_articles/webhose_english_dataset/$article" "${dataset_dir_name}/${article}";
 	((i++));
 	if [[ "$i" -gt "${NUM_ARTICLES}" ]]; then
 		break;
