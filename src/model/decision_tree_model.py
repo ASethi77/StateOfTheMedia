e27bc5126a9d26dcd9ce8538bebb3207aa999d1c
Aakash Sethi <aakash.k.sethi@gmail.com>
2017-04-18 17:31:13 -0700

    Added script to print k-fold loss for different decision tree max depths

--- src/model/decision_tree_model.py
+++ src/model/decision_tree_model.py
@@ -5,23 +5,23 @@ from sklearn.tree import DecisionTreeRegressor
 class DecisionTreeRegressionModel(RegressionModel):
 	def __init__(self, train_data, max_depth=2):
 		RegressionModel.__init__(self, train_data)
-		self._model = DecisionTreeRegressor(max_depth=max_depth)
+		self.model = DecisionTreeRegressor(max_depth=max_depth)
 
 	def train(self, x=None, y=None):
 		x = x if x is not None else self.train_x
 		y = y if y is not None else self.train_y
 
-		self._model.fit(x, y)
+		self.model.fit(x, y)
 
 	def predict(self, x_in):
-		return self._model.predict(x_in)
+		return self.model.predict(x_in)
 
 	def evaluate(self, x_in, y_out):
-		raise NotImplementedError
+		return self.model.score(x_in, y_out)
 
 	def save(self, filename):
-		joblib.dump(self._model, filename)
+		joblib.dump(self.model, filename)
 
 	def load(self, filename):
-		return joblib.load(filename)
+		self.model = joblib.load(filename)
 
--- src/model/dev_corpus_decision_tree_regression.py
+++ src/model/dev_corpus_decision_tree_regression.py
@@ -1,6 +1,9 @@
 import model.feature_util
 from preprocess_text.setup_corpus import setup_dev_corpus
 from model.decision_tree_model import DecisionTreeRegressionModel
+from model.decision_tree_model import DecisionTreeRegressionModel
+
+from sklearn.model_selection import cross_val_score
 
 if __name__ == '__main__':
 	dev_corpus_100_articles = setup_dev_corpus()
@@ -11,7 +14,7 @@ if __name__ == '__main__':
 	bag_of_words_train, bag_of_words_test, approval_ratings_train, approval_ratings_test = \
 			model.feature_util.train_test_split(bag_of_words_per_day, presidential_approval_ratings)
 
-	dev_corpus_regression_model = DecisionTreeRegressionModel([bag_of_words_train, approval_ratings_train])
+	dev_corpus_regression_model = DecisionTreeRegressionModel([bag_of_words_train, approval_ratings_train], max_depth=2)
 	dev_corpus_regression_model.train()
 
 	bag_of_words_sanity = bag_of_words_train[0]
@@ -21,3 +24,14 @@ if __name__ == '__main__':
 	print("Sanity checking regression on trained example")
 	print("Predicted approval ratings:\n\tApprove: {0}%\n\tDisapprove: {1}%".format(approval_rating_prediction[0], approval_rating_prediction[1]))
 	print("Actual approval ratings:\n\tApprove: {0}%\n\tDisapprove: {1}%".format(approval_rating_sanity[0], approval_rating_sanity[1]))
+
+	print("Sanity check on training example:")
+	print(dev_corpus_regression_model.evaluate(bag_of_words_test, approval_ratings_test))
+
+	print("Computing k-fold loss with different max decision tree depths:")
+	depth_min = 1
+	depth_max = 25
+	for i in range(depth_min, depth_max + 1, 1):
+		print(i)
+		dev_corpus_regression_model = DecisionTreeRegressionModel([bag_of_words_train, approval_ratings_train], max_depth=i)
+		print(cross_val_score(dev_corpus_regression_model.model, bag_of_words_per_day, presidential_approval_ratings, n_jobs=-1, cv=4))
