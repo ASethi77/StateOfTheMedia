665baf70067587d38666156febdf0dd1fefc3f8d
Aakash Sethi <aakash.k.sethi@gmail.com>
2017-04-25 14:18:45 -0700

    Removed unnecessary python scripts

--- src/preprocess_text/articles_to_corpus.py
+++
@@ -1,8 +0,0 @@
-import textacy
-articles = []
-for filename in textacy.fileio.utils.get_filenames('../../data/Articles/'):
-    article = textacy.fileio.read_file(filename, mode=u'rt', encoding=None)
-    articles.append(article)
-
-article_corpus = textacy.Corpus('en', texts=articles)
-article_corpus.save(path='../../data/TextacyCorpra/', name='temp_corpus')
