e268d07ed1b15cbe34007ab360f52d09c1305388
Aakash Sethi <aakash.k.sethi@gmail.com>
2017-05-02 16:09:34 -0700

    Made setup corpus work with nyt dataset

--- src/preprocess_text/article_parsers/nyt_article_parser.py
+++ src/preprocess_text/article_parsers/nyt_article_parser.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import json
 import textacy
 from .article_parser import ArticleParser
 from preprocess_text.document import Document
@@ -7,19 +8,23 @@ from preprocess_text.document import Document
 class NYTArticleParser(ArticleParser):
     def __init__(self, articles_directory):
         ArticleParser.__init__(self, articles_directory)
+        self.json_article_files = glob.iglob(os.path.join(os.path.abspath(self.articles_dir), "*.json"))
+
+    def num_articles(self):
+        return len(glob.glob1(os.path.join(os.path.abspath(self.articles_dir)), "*.json"))
 
     def yield_articles(self):
-        json_article_files = glob.iglob(os.path.join(os.path.abspath(self.articles_dir), "*.json"))
-        for article_filename in json_article_files:
-            article_json = textacy.fileio.read.read_json(article_filename)
-            if body not in article_json.keys():
+        for article_filename in self.json_article_files:
+            article_contents = ''
+            with open(article_filename, "r") as article:
+                article_contents = ' '.join(article.readlines())
+
+            article_json = json.loads(article_contents.strip())
+            if 'body' not in article_json:
                 continue
-            content, metadata = textacy.fileio.utils.split_record_fields(article_json, "body")
-            content_full = ""
-            for line in content:
-                content_full += line.encode('ascii', errors='replace').decode('ascii')
-            metadata_full = {}
-            for data in metadata:
-                metadata_full = {**metadata_full, **data}
-            doc = Document(content_full, metadata=metadata_full, lang="en")
+            content = article_json["body"]
+            del article_json["body"]
+            metadata = article_json
+            doc = Document(content, metadata=metadata, lang="en")
+            doc.metadata["published"] = doc.metadata["publicationDate"]
             yield doc
--- src/preprocess_text/setup_corpus.py
+++ src/preprocess_text/setup_corpus.py
@@ -19,9 +19,9 @@ def setup_corpus(article_parser_type, article_dir, corpus_name, max_articles, pe
     print(type(myparser))
     print(article_parser_type)
     # where doc is a textacy doc object
+    num_articles = myparser.num_articles()
     for doc in myparser.yield_articles():
         if (doc.n_sents >= min_length): # ignore junk news (articles that are too short)
-            doc.metadata["published"] = doc.metadata["publicationDate"]
             i += 1
             print("Processing doc #: " + str(i))
             if per_date:
@@ -31,7 +31,7 @@ def setup_corpus(article_parser_type, article_dir, corpus_name, max_articles, pe
             else:
                 doc_list.append(doc)
 
-        print("Adding articles to corpus: {0}% complete ({1} of {2}).\r".format(i * 100.0 / max_articles, i, max_articles))
+        print("Adding articles to corpus: {0}% complete ({1} of {2}).\r".format(i * 100.0 / num_articles, i, num_articles))
         sys.stdout.flush()
 
         if i >= max_articles:
@@ -62,7 +62,7 @@ def setup_dev_corpus(corpus_name="WebHoseDevCorpus", max_articles=100, per_date=
     return setup_corpus(WebhoseArticleParser, "../data/Articles/WebHoseDevCorpus", corpus_name, max_articles, per_date, use_big_data)
 
 def setup_nyt_corpus():
-    return setup_corpus(NYTArticleParser, "/opt/nlp_shared/data/news_articles/nytimes/nytimes_json/", "NYTCorpus", 1e8, True, True, corpus_dir="/opt/nlp_shared/corpora/NytCorpora")
+    return setup_corpus(NYTArticleParser, "/opt/nlp_shared/data/news_articles/nytimes/nytimes_json/", "NYTCorpus", 1e8, True, True, corpus_dir="/opt/nlp_shared/corpora/NytCorpora", min_length=1)
 
 def setup_100M_corpus(corpus_name="HundredMegsCorpus", per_date=True, use_big_data=True):
     return setup_corpus(WebhoseArticleParser, "/opt/nlp_shared/data/news_articles/webhose_english_dataset/WebHoseDataset-16275-Articles", corpus_name, 17000, per_date, use_big_data)
