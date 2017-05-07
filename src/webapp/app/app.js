6c3fa43250ba8eb58bcc1be963d7438507ac354a
Aakash Sethi <Aakash Sethi>
2017-05-07 05:59:05 -0700

    Completed sentiment analysis mockup + emotion image binding

--- src/webapp/app/app.js
+++ src/webapp/app/app.js
@@ -25,14 +25,16 @@ var StateOfTheMediaController = (function () {
         this.$scope = null;
         this.$http = null;
         this.articlesPerDay = {};
+        this.sentimentPerDay = {};
         this.possibleExpressions = {
             "Really Happy": new FacialExpression("Really Happy", "./img/ReallyHappy.jpg"),
             "Happy": new FacialExpression("Happy", "./img/Happy.jpg"),
             "Sad": new FacialExpression("Sad", "./img/Sad.jpg"),
-            "Really Sad": new FacialExpression("Really Sad", "./img/ReallySad.jpg")
+            "Really Sad": new FacialExpression("Really Sad", "./img/ReallySad.jpg"),
+            "Neutral": new FacialExpression("Neutral", "./img/Neutral.jpg")
         };
-        this.currentExpression = null;
-        this.showExpression = false;
+        this.currentExpression = this.possibleExpressions["Neutral"];
+        this.showExpression = true;
         this.addArticle = function (content, date) {
             var article = new Article(content, date);
             var dateKey = date.toDateString();
@@ -44,30 +46,52 @@ var StateOfTheMediaController = (function () {
                 articleList = [];
             }
             articleList.push(article);
-            this.$http({
-                method: "POST",
-                data: articleList,
-                url: "/sentiments"
-            }).then(function success(response) {
-                console.log(response);
-            }, function error(response) {
-                console.log(response);
-            });
-        };
-        this.hideExpression = function () {
-            this.showExpression = false;
-        };
-        this.displayExpression = function () {
-            this.showExpression = true;
-        };
-        this.setExpression = function (newExpression) {
-            this.hideExpression();
-            this.currentExpression = newExpression;
-            this.displayExpression();
+            this.updateSentimentForDate(date);
         };
         this.$scope = $scope;
         this.$http = $http;
+        // default date for demo
+        this.$scope.articleDate = new Date(2016, 8, 21);
     }
+    StateOfTheMediaController.prototype.updateSentimentForDate = function (day) {
+        var sentimentPerDay = this.sentimentPerDay;
+        var dateKey = day.toDateString();
+        var articleList = this.articlesPerDay[dateKey];
+        var controller = this;
+        this.$http({
+            method: "POST",
+            data: angular.toJson(articleList),
+            url: "/sentiments"
+        }).then(function success(response) {
+            var sentiment = angular.fromJson(response.data);
+            sentimentPerDay[dateKey] = sentiment;
+            controller.currentExpression = controller.sentimentToFacialExpression(sentiment);
+        }, function error(response) {
+            console.log(response);
+        });
+    };
+    ;
+    StateOfTheMediaController.prototype.sentimentToFacialExpression = function (sentiment) {
+        if (sentiment >= 0.0 && sentiment < 0.20) {
+            return this.possibleExpressions["Really Sad"];
+        }
+        else if (sentiment >= 0.20 && sentiment < 0.40) {
+            return this.possibleExpressions["Sad"];
+        }
+        else if (sentiment >= 0.40 && sentiment < 0.60) {
+            return this.possibleExpressions["Neutral"];
+        }
+        else if (sentiment >= 0.60 && sentiment < 0.80) {
+            return this.possibleExpressions["Happy"];
+        }
+        else if (sentiment >= 0.80 && sentiment <= 1.0) {
+            return this.possibleExpressions["Really Happy"];
+        }
+        else {
+            console.error("Unable to determine appropriate expression for " + sentiment.toPrecision(3));
+            return this.currentExpression;
+        }
+    };
     return StateOfTheMediaController;
 }());
 StateOfTheMediaController.AngularDependencies = ['$scope', '$http', StateOfTheMediaController];
@@ -79,9 +103,8 @@ var app = angular.module('StateOfTheMediaApp', [
     'ngMockE2E'
 ]);
 app.run(['$httpBackend', function ($httpBackend) {
-        var sentiment = 25;
-        console.log("apprunner");
         $httpBackend.whenPOST("/sentiments").respond(function (method, url, data) {
+            var sentiment = Math.random();
             return [200, sentiment, {}];
         });
     }]);
--- src/webapp/app/app.ts
+++ src/webapp/app/app.ts
@@ -31,24 +31,29 @@ class StateOfTheMediaController {
     // injected similar to the todomvc typescript + angular example on GH
     // <see https://github.com/tastejs/todomvc/blob/gh-pages/examples/typescript-angular/js/controllers/TodoCtrl.ts">
     public $scope: ng.IScope = null;
-    public $http: ng.IModule = null;
+    public $http = null;
 
     constructor($scope: ng.IScope, $http: ng.IModule) {
         this.$scope = $scope;
         this.$http = $http;
+
+        // default date for demo
+        this.$scope.articleDate = new Date(2016, 8, 21);
     }
 
-    private articlesPerDay:  { [day: string]: number[] } = {};
+    private articlesPerDay:  { [day: string]: Article[] } = {};
+    private sentimentPerDay: { [day: string]: number } = {};
 
     private possibleExpressions: { [name: string]: FacialExpression } = {
         "Really Happy": new FacialExpression("Really Happy", "./img/ReallyHappy.jpg"),
         "Happy": new FacialExpression("Happy", "./img/Happy.jpg"),
         "Sad": new FacialExpression("Sad", "./img/Sad.jpg"),
-        "Really Sad": new FacialExpression("Really Sad", "./img/ReallySad.jpg")
+        "Really Sad": new FacialExpression("Really Sad", "./img/ReallySad.jpg"),
+        "Neutral": new FacialExpression("Neutral", "./img/Neutral.jpg")
     };
 
-    public currentExpression: FacialExpression = null;
-    public showExpression: boolean = false;
+    public currentExpression: FacialExpression = this.possibleExpressions["Neutral"];
+    public showExpression: boolean = true;
 
     public addArticle = function (content: string, date: Date)
     {
@@ -63,33 +68,44 @@ class StateOfTheMediaController {
         }
         articleList.push(article);
 
+        this.updateSentimentForDate(date);
+    };
+
+    public updateSentimentForDate(day: Date)
+    {
+        var sentimentPerDay = this.sentimentPerDay;
+        var dateKey = day.toDateString();
+        var articleList: Article[] = this.articlesPerDay[dateKey];
+
+        var controller = this;
         this.$http({
             method: "POST",
-            data: articleList,
+            data: angular.toJson(articleList),
             url: "/sentiments"
         }).then(function success(response) {
-                console.log(response);
+                var sentiment: number = angular.fromJson(response.data);
+                sentimentPerDay[dateKey] = sentiment;
+                controller.currentExpression = controller.sentimentToFacialExpression(sentiment);
            }, function error(response) {
                 console.log(response);
         });
     };
 
-    private hideExpression = function ()
-    {
-        this.showExpression = false;
-    };
-
-    private displayExpression = function ()
-    {
-        this.showExpression = true;
-    };
-
-    private setExpression = function (newExpression: FacialExpression)
-    {
-        this.hideExpression();
-        this.currentExpression = newExpression;
-        this.displayExpression();
-    };
+    public sentimentToFacialExpression(sentiment: number) {
+        if (sentiment >= 0.0 && sentiment < 0.20) {
+            return this.possibleExpressions["Really Sad"];
+        } else if (sentiment >= 0.20 && sentiment < 0.40) {
+            return this.possibleExpressions["Sad"];
+        } else if (sentiment >= 0.40 && sentiment < 0.60) {
+            return this.possibleExpressions["Neutral"];
+        } else if (sentiment >= 0.60 && sentiment < 0.80) {
+            return this.possibleExpressions["Happy"];
+        } else if (sentiment >= 0.80 && sentiment <= 1.0) {
+            return this.possibleExpressions["Really Happy"];
+        } else {
+            console.error("Unable to determine appropriate expression for " + sentiment.toPrecision(3));
+            return this.currentExpression;
+        }
 }
 
 var app = angular.module('StateOfTheMediaApp', [
@@ -97,9 +113,8 @@ var app = angular.module('StateOfTheMediaApp', [
 ]);
 
 app.run(['$httpBackend', function ($httpBackend) {
-    var sentiment: number = 25;
-    console.log("apprunner");
     $httpBackend.whenPOST("/sentiments").respond(function (method, url, data) {
+        var sentiment = Math.random();
         return [200, sentiment, {}];
     });
 }]);
