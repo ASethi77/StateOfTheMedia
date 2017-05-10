ab05a982afe35428f611ba301d575da041a3c175
Kevin Liang <kevinl95@yahoo.com>
2017-05-09 17:20:22 -0700

    Added Multi-Layer Perceptron Model

---
+++ src/model/MLPRegressionModel.py
@@ -0,0 +1,26 @@
+from .regression_model import RegressionModel
+from sklearn.externals import joblib
+from sklearn.neural_network import MLPRegressor
+
+class MLPRegressionModel(RegressionModel):
+    def __init__(self, train_data):
+        RegressionModel.__init__(self, train_data)
+        self.model = MLPRegressor()
+
+    def train(self, x=None, y=None):
+        x = x if x is not None else self.train_x
+        y = y if y is not None else self.train_y
+        
+        self.model.fit(x, y)
+    
+    def predict(self, x_in):
+        return self.model.predict(x_in)
+
+    def evaluate(self, x_in, y_out):
+        return self.model.score(x_in, y_out)
+
+    def save(self, filename):
+        joblib.dump(self.model, filename)
+
+    def load(self, filename):
+        self.model = joblib.load(filename)
