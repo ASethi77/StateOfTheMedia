ea14d796ff385b073412cff130915a31e6ab5655
dowling3 <drewdowling37@gmail.com>
2017-05-16 18:40:16 -0700

    fixed model loading and saving. Also testing is for NERDS

--- src/model/linear_regression_model.py
+++ src/model/linear_regression_model.py
@@ -1,20 +1,22 @@
 from .regression_model import RegressionModel
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 class LinearRegressionModel(RegressionModel):
-	def __init__(self, train_data):
-		RegressionModel.__init__(self, train_data)
-		self.model = LinearRegression()
+    def __init__(self, train_data):
+        RegressionModel.__init__(self, train_data)
+        self.model = LinearRegression()
 
-	def train(self, x=None, y=None):
-		x = x if x is not None else self.train_x
-		y = y if y is not None else self.train_y
+    def train(self, x=None, y=None):
+        x = x if x is not None else self.train_x
+        y = y if y is not None else self.train_y
 
-		self.model.fit(x, y)
+        self.model.fit(x, y)
 
-	def predict(self, x_in):
-		return self.model.predict(x_in)
+    def predict(self, x_in):
+        return self.model.predict(x_in)
 
-	def evaluate(self, x_in, y_out):
-		return self.model.score(x_in, y_out)
+    def evaluate(self, x_in, y_out):
+        predicted = self.predict(x_in)
+        return mean_squared_error(y_out, predicted)
 
