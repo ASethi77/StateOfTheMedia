3354b2dccb1e9d6b06e7d0cfb23af4748530060d
Aakash Sethi <aakash.k.sethi@gmail.com>
2017-05-22 21:18:41 -0700

    Fixed bug in preprocessors/corpora loaders

--- src/preprocess_text/load_corpora.py
+++ src/preprocess_text/load_corpora.py
@@ -7,8 +7,8 @@ from .corpus import Corpus
 
 def load_corpora(corpus_name, corpus_dir="../data/", years=[]):
     corpus_path = os.path.join(corpus_dir, corpus_name)
-    if corpus_path[-1] != "/":
-        corpus_path += "/"
+    if corpus_path[-1] != os.sep:
+        corpus_path += os.sep
 
     if len(years) == 0:
         with open(os.path.join(corpus_path, "dates.json"), "rb") as dates_file:
@@ -24,12 +24,14 @@ def load_corpora(corpus_name, corpus_dir="../data/", years=[]):
     else:
         print("Loading corpora for years between {0} and {1}".format(years[0], years[-1]))
         corpora = {}
+
         for year in range(years[0], years[-1]):
             corpora_files_for_year = glob.glob(os.path.join(corpus_path, "{}*industrial*".format(year)))
             corpora_for_year = 0
             for corpora_file in corpora_files_for_year:
-                corpus_date = corpora_file.split('/')[-1][:10]
+                corpus_date = corpora_file.split(os.sep)[-1][:10]
                 corpus_for_date = Corpus.load(os.path.dirname(corpora_file), corpus_date)
+                corpus_for_date.docs.temp_docs_dir = corpus_path
                 corpora[parse(corpus_date)] = corpus_for_date
                 corpora_for_year += 1
             print("Found articles for {} days in the year {}".format(corpora_for_year, year))
